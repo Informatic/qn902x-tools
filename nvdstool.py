99476449d0444480d6aec39bcda19d116ab8ce61
Piotr Dobrowolski <admin@tastycode.pl>
2016-04-06 11:42:47 +0200

    Implement NVDS writing and more commands

--- nvdstool.py
+++ nvdstool.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-QN902x NVDS tool
+QN902x programming tool
 Copyright (c) 2016 Piotr Dobrowolski
 
 Permission is hereby granted, free of charge, to any person obtaining
@@ -30,7 +30,7 @@ import logging
 import time
 import argparse
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
@@ -72,7 +72,7 @@ class QNClient(object):
         # This is special, don't touch this
         self.ser.write('\x33')
         self.send_command(0x34, '\x2c\x08\x00\x00')
-        self.read_packet()
+        self.read_packet(True)
 
         self.ser.close()
         self.ser.baudrate = self.baudrate
@@ -80,7 +80,7 @@ class QNClient(object):
         self.ser.open()
 
         self.send_command(0x34, '\x2c\x08\x00\x00')
-        self.read_packet()
+        self.read_packet(True)
 
     @classmethod
     def build_packet(self, cmd, data):
@@ -88,56 +88,107 @@ class QNClient(object):
         checksum = crc16.crc16xmodem(msg)
         return '\x71' + msg + struct.pack('<H', checksum)
 
-    def send_command(self, cmd, data):
+    def send_command(self, cmd, data=''):
         logger.debug('Sending %02X command: %r', cmd, data)
         self.ser.write(self.build_packet(cmd, data))
         self.ser.flush()
 
-    def read_packet(self):
+    def read_packet(self, only_confirm=False):
         sync = self.ser.read(1)
-        if sync != '\x01':
+        if sync != '\x01':  # 0x01 == success, 0x02 == failure
             raise Exception('0x01 expected, got %r' % sync)
 
-        header = self.ser.read(5)
+        if only_confirm:
+            return
 
-        if not header:
-            # No header received
-            return None
+        self.ser.timeout = 5
+        startbyte = self.ser.read(1)
 
-        startbyte, cmd, data_len = struct.unpack('<cbHx', header)
+        logger.debug('Got response (%r)', startbyte)
+
+        if not startbyte:
+            raise Exception('No start byte received!')
+
+        if startbyte in ('\x03', '\x04'):
+            # Single byte response
+            # 0x03 == success, 0x04 == failure
+            return ord(startbyte)
+
+        header = self.ser.read(4)
+        self.ser.timeout = 0.5
+
+        cmd, data_len = struct.unpack('<bHx', header)
 
         if startbyte != '\x71':
             raise Exception('0x71 expected, got %r' % startbyte)
 
         data = self.ser.read(data_len)
+        logger.debug('Response read - %d bytes', data_len)
 
-        crc = crc16.crc16xmodem(header[1:] + data)
+        crc = crc16.crc16xmodem(header + data)
         if self.ser.read(2) != struct.pack('<H', crc):
             raise Exception('Invalid checksum')
 
         return data
 
     def read_nvds(self):
-        self.send_command(0x3b, '\x00\x00\x00\x00')
-        self.read_packet()
+        self.set_program_address(0)
 
         nvds_data = ''
         for n in range(16):
             self.send_command(0x46, '\x00\x01\x00\x00')
             nvds_data += self.read_packet()
 
-        if nvds_data[:4] != 'NVDS':
-            logger.warning('No NVDS header found')
+        try:
+            self.validate_nvds(nvds_data)
+        except:
+            logger.warning('Invalid NVDS data', exc_info=True)
 
         return nvds_data
 
+    def validate_nvds(self, nvds_data):
+        if nvds_data[:4] != 'NVDS':
+            raise Exception('Missing NVDS header')
+
+        if len(nvds_data) != 4096:
+            raise Exception('Invalid NVDS data length')
+
     def reboot(self):
-        self.send_command(0x4a, '')
-        self.read_packet()
+        self.send_command(0x4a)
+        return self.read_packet(True)
+
+    def set_load_target(self, target):
+        """Changes load target, 1 equals flash, 0 equals SRAM"""
+        self.send_command(0x39, struct.pack('<L', int(target)))
+        return self.read_packet(True)
+
+    def get_bootloader_version(self):
+        self.send_command(0x36)
+        return self.read_packet()
+
+    def get_chip_id(self):
+        self.send_command(0x37)
+        return self.read_packet()
+
+    def get_flash_id(self):
+        self.send_command(0x38)
+        return self.read_packet()
+
+    def program(self, data):
+        self.send_command(0x45, data)
+        return self.read_packet()
+
+    def set_program_address(self, address):
+        self.send_command(0x3b, struct.pack('<L', address))
+        return self.read_packet(True)
+
+    def sector_erase(self, sector):
+        self.send_command(0x42, struct.pack('<L', sector))
+        return self.read_packet()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='NVDS tool for Quintic QN902x BLE SoC'
+        description='Programming tool for Quintic QN902x BLE SoC'
         )
 
     parser.add_argument('--port', dest='port', default='/dev/ttyUSB0')
@@ -149,7 +200,15 @@ if __name__ == "__main__":
     group.add_argument('-W', '--write', dest='write_fname',
                        help='Write NVDS data from file')
 
+    parser.add_argument('-f', '--force', dest='force', action='store_true',
+                        help='Force write of possibly invalid data')
+    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
+                        help='xD')
+
     args = parser.parse_args()
+
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
 
     client = QNClient(args.port, args.baudrate)
     client.connect()
@@ -160,6 +219,26 @@ if __name__ == "__main__":
         logger.info('NVDS data saved to %s', args.read_fname)
 
     elif args.write_fname:
-        raise NotImplemented
+        with open(args.write_fname, 'r') as fd:
+            nvds_data = fd.read()
+
+            try:
+                client.validate_nvds(nvds_data)
+            except:
+                logger.warning('Invalid NVDS data', exc_info=True)
+
+                if not args.force:
+                    exit(1)
+
+            logger.info('Erasing...')
+            client.set_program_address(0)
+            client.sector_erase(1)
+
+            client.set_program_address(0)
+            for n in range(0, len(nvds_data), 256):
+                logger.info('Programming... %.2f%%', 100.0*n/len(nvds_data))
+                client.program(nvds_data[n:n+256])
+
+            logger.info('Finished.')
 
     client.reboot()
